fe39f9676c9f2f0b9898308c1f4a81594dbb5e18
Ismail Sharkawy <ismail@aucegypt.edu>
2021-10-10 13:53:31 +0200

    adding pages app,creating pages urls, adding pages templates

---
+++ pages/views.py
@@ -0,0 +1,13 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+
+# Create your views here.
+
+
+def index(request):
+    return render(request, "pages/index.html")
+
+
+def about(request):
+    return render(request, "pages/about.html")
